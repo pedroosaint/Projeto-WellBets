7f5265f2654d886b5792606d2887eea433ce5daf
Pedro <pedrooliveiradossantos2010@gmail.com>
2026-06-24 10:40:46 -0300

    feat: Criando o JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// DEFINE O OBJETO COM OS RESULTADOS(BANCO)
+const placarOficial = {
+    1: {time1:2, time2:1},
+    2: {time1:3, time2:0},
+}
+
+//Fução SALVAR PALPITE
+function salvarPalpite(nomeJogo,id){
+   
+    //CAPTURA ELEMENTOS DO DOM
+    const input1 = document.getElementById(`time1-${id}`);
+    const input2 = document.getElementById(`time2-${id}`);
+
+    //VALIDAÇÃO DOS CAMPOS
+    
+}
